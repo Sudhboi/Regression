50267f8e4799e02e340f77eb6fb96a7f6256ebd6
Sudhboi <sudhboiii@gmail.com>
2025-04-17 08:55:47 +0530

    finished linear regression

--- Linear Regression/linreg.py
+++ Linear Regression/linreg.py
@@ -1,13 +1,50 @@
 import csv
+import matplotlib.pyplot as plt
+import numpy as np
 
-xset = yset = []
+xset = []
+yset = []
 
-fh = open("dataset.csv", newline="")
+fh = open("Linear Regression\dataset.csv", newline="")
 reader = csv.reader(fh)
 for i in reader:
-    xset.append(i[0])
-    yset.append(i[1])
+    xset.append(float(i[0]))
+    yset.append(float(i[1]))
 fh.close()
 
-def error(m, c):
-    pass
+n = len(xset)
+p = 0.000000001
+tries = 10000
+
+def meanError(m, c):
+    error = 0
+    for i in range(0, n):
+        error += (m*xset[i] + c - yset[i]) ** 2
+    merror = error/n
+    return merror
+
+def pderm(m, c):
+    temp = 0
+    for i in range(n):
+        temp += (m*xset[i] + c - yset[i]) * xset[i]
+    return (2/n) * temp
+
+def pderc(m, c):
+    temp = 0
+    for i in range(n):
+        temp += (m*xset[i] + c - yset[i])
+    return (2/n) * temp
+
+m = c = 1
+
+for i in range(tries):
+    print("Mean Error:", meanError(m, c))
+    m -= p * pderm(m, c)
+    c -= p * pderc(m, c)
+    print(m, c)
+
+x = np.linspace(min(xset), max(xset) + 10, 101)
+y = m*x + c
+plt.plot(xset, yset, 'o')
+plt.plot(x, y)
+plt.show()
